5d00fe4bc263c1a8d3dc1d08b892c8fcc6453752
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2024-03-31 01:35:08 -0400

    pg: use environment namespace when rendering chart

--- cmd/pg/main.go
+++ cmd/pg/main.go
@@ -16,7 +16,7 @@ func main() {
 }
 
 func run() error {
-	resources, err := postgresql.RenderChart(os.Args[0], "", &postgresql.Values{})
+	resources, err := postgresql.RenderChart(os.Args[0], os.Getenv("NAMESPACE"), &postgresql.Values{})
 	if err != nil {
 		return fmt.Errorf("failed to render postgres chart: %w", err)
 	}
