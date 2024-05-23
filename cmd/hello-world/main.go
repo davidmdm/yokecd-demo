0d8f7eab6822629b92d1296ba8f3509564a10514
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2024-05-23 17:54:48 -0400

    hello-world: more idiomatic

--- cmd/hello-world/main.go
+++ cmd/hello-world/main.go
@@ -6,6 +6,10 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/davidmdm/yoke/pkg/flight"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	appsv1 "k8s.io/client-go/applyconfigurations/apps/v1"
 	corev1 "k8s.io/client-go/applyconfigurations/core/v1"
 	metav1 "k8s.io/client-go/applyconfigurations/meta/v1"
@@ -19,32 +23,81 @@ func main() {
 }
 
 func run() error {
-	name := os.Args[0] + "-sample-app"
-	labels := map[string]string{"app": name}
+	var (
+		release   = flight.Release()
+		namespace = flight.Namespace()
+		labels    = map[string]string{"app": release}
+	)
 
 	replicas := flag.Int("replicas", 3, "desired replica count for deployment")
-
 	flag.Parse()
 
+	resources := []any{
+		CreateDeployment(DeploymentConfig{
+			Name:      release,
+			Namespace: namespace,
+			Labels:    labels,
+			Replicas:  int32(*replicas),
+		}),
+		CreateService(ServiceConfig{
+			Name:       release,
+			Namespace:  namespace,
+			Labels:     labels,
+			Port:       80,
+			TargetPort: 80,
+		}),
+	}
+
+	return json.NewEncoder(os.Stdout).Encode(resources)
+}
+
+type DeploymentConfig struct {
+	Name      string
+	Namespace string
+	Labels    map[string]string
+	Replicas  int32
+}
+
+func CreateDeployment(cfg DeploymentConfig) *appsv1.DeploymentApplyConfiguration {
 	container := corev1.Container().
-		WithName(name).
+		WithName(cfg.Name).
 		WithImage("alpine:latest").
 		WithCommand("watch", "echo", "hello", "world")
 
 	podTemplate := corev1.PodTemplateSpec().
-		WithLabels(labels).
+		WithLabels(cfg.Labels).
 		WithSpec(corev1.PodSpec().WithContainers(container))
 
 	spec := appsv1.DeploymentSpec().
-		WithReplicas(int32(*replicas)).
-		WithSelector(metav1.LabelSelector().WithMatchLabels(labels)).
-		WithTemplate(
-			podTemplate,
-		)
+		WithReplicas(cfg.Replicas).
+		WithSelector(metav1.LabelSelector().WithMatchLabels(cfg.Labels)).
+		WithTemplate(podTemplate)
 
-	deployment := appsv1.Deployment(name, os.Getenv("NAMESPACE")).
-		WithLabels(labels).
+	return appsv1.Deployment(cfg.Name, cfg.Namespace).
+		WithLabels(cfg.Labels).
 		WithSpec(spec)
+}
 
-	return json.NewEncoder(os.Stdout).Encode(deployment)
+type ServiceConfig struct {
+	Name       string
+	Namespace  string
+	Labels     map[string]string
+	Port       int32
+	TargetPort int
+}
+
+func CreateService(cfg ServiceConfig) *corev1.ServiceApplyConfiguration {
+	servicePort := corev1.ServicePort().
+		WithProtocol(v1.ProtocolTCP).
+		WithPort(cfg.Port).
+		WithTargetPort(intstr.FromInt(cfg.TargetPort))
+
+	serviceSpec := corev1.ServiceSpec().
+		WithSelector(cfg.Labels).
+		WithType(v1.ServiceTypeClusterIP).
+		WithPorts(servicePort)
+
+	return corev1.Service(cfg.Name, cfg.Namespace).
+		WithLabels(cfg.Labels).
+		WithSpec(serviceSpec)
 }
